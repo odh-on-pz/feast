047fc2c9318621f01534b54fc75d266a365e1876
puneetsharma21 <puneet.sharma21@ibm.com>
2025-09-22 04:00:28 -0400

    perl&ncurses

--- sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
+++ sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
@@ -9,8 +9,9 @@ CMAKE_VERSION=3.30.5
 CMAKE_REQUIRED_VERSION=3.30.5
 
 dnf repolist enabled
-dnf --showduplicates list perl ncurses-devel
-dnf install -y perl ncurses-devel
+# dnf --showduplicates list perl ncurses-devel
+dnf --showduplicates list perl ncurses-devel perl-libs perl-interpreter ncurses-libs
+dnf install -y perl ncurses-devel perl-libs perl-interpreter ncurses-libs
 dnf install -y gcc-toolset-13 make cmake ninja-build libomp-devel \
                git python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip \
                openssl openssl-devel zlib-devel libuuid-devel 
