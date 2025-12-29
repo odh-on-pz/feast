a6a8cb1c9bc381c80881d104708bf7572b8fb0ec
puneetsharma21 <puneet.sharma21@ibm.com>
2025-12-29 06:15:24 -0500

    commenting rapidjson subpackage

--- sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
+++ sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
@@ -126,16 +126,16 @@ cd $WORKDIR
 
 
 ################## rapidjson installing ########################
-echo "Entering rapidson source directory..."
-cd rapidjson
-mkdir build && cd build
-echo "Running cmake to configure the rapidjson build..."
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
-echo "Compiling the source code for rapidjson..."
-make -j$(nproc)
-echo "Installing rapidjson"
-make install
-cd $WORKDIR
+# echo "Entering rapidson source directory..."
+# cd rapidjson
+# mkdir build && cd build
+# echo "Running cmake to configure the rapidjson build..."
+# cmake .. -DCMAKE_INSTALL_PREFIX=/usr/local
+# echo "Compiling the source code for rapidjson..."
+# make -j$(nproc)
+# echo "Installing rapidjson"
+# make install
+# cd $WORKDIR
 
 ################### xsimd installing ############################
 echo "Entering xsimd source directory..."
