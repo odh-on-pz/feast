02ab84b858f2d9a24cda1fec574f3a2b8b8e363b
puneetsharma21 <puneet.sharma21@ibm.com>
2026-01-01 11:50:23 -0500

    building rapidjson

--- sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
+++ sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
@@ -1,3 +1,34 @@
+#!/bin/bash
+set -Eeuo pipefail
+trap 'echo "[prebuild-power] failed at line $LINENO"; exit 1' ERR
+shopt -s dotglob nullglob
+
+PYTHON_VERSION=3.11
+WORKDIR=$(pwd)
+CMAKE_VERSION=3.30.5
+CMAKE_REQUIRED_VERSION=3.30.5
+
+: "${CFLAGS:=}"
+: "${CXXFLAGS:=}"
+: "${LDFLAGS:=}"
+: "${LINKFLAGS:=}"
+: "${CC_FOR_BUILD:=}"
+: "${CXX_FOR_BUILD:=}"
+: "${CMAKE_ARGS:=}"
+: "${CPPFLAGS:=}"
+: "${VIRTUAL_ENV_PATH:=}"
+: "${CONDA_BUILD_CROSS_COMPILATION:=}"
+: "${PREFIX:=}"
+: "${FFLAGS:=}"
+
+dnf install -y gcc-toolset-13 make cmake ninja-build libomp-devel \
+               git python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip \
+               openssl openssl-devel zlib-devel libuuid-devel lz4-devel libtool
+
+# Enable GCC toolset
+source /opt/rh/gcc-toolset-13/enable
+export CXX=/opt/rh/gcc-toolset-13/root/usr/bin/g++
+
 echo "Entering rapidson source directory..."
 cd rapidjson
 mkdir build && cd build
